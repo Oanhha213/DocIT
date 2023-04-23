40d1af520b8d6e93afab826ad3705a55b89a5f19
Oanhha213 <oanhha213@gmai.com>
2023-04-23 22:06:37 +0700

    ltapi

---
+++ LTAPI/API2/API2/Controllers/CustomersController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API2.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        //httpGet: dùng để lấy thông tin khách hàng
+        //1. Dịch vụ lấy thông tin của toàn bộ khách hàng
+        [HttpGet]
+        public List<tblKhach2> GetCustomerLists()
+        {
+            DBCustomersDataContext dbCustomer = new DBCustomersDataContext("");
+            return dbCustomer.tblKhach2s.ToList();
+        }
+        //2. Dịch vụ lấy thông tin một khách hàng với mã nào đó
+         [HttpGet]
+ public tblKhach2 GetCustomer(string id)
+        {
+            DBCustomersDataContext dbCustomer = new DBCustomersDataContext("");
+            return dbCustomer.tblKhach2s.FirstOrDefault(x =>
+           x.Makhach == id);
+        }
+        //3. httpPost, dịch vụ thêm mới một khách hàng
+        [HttpPost]
+        public bool InsertNewCustomer(string id, string name,
+       string adress, string phoneNumber)
+        {
+            try
+            {
+                DBCustomersDataContext dbCustomer = new DBCustomersDataContext("");
+                tblKhach2 customer = new tblKhach2();
+                customer.Makhach = id;
+                customer.TenKhach = name;
+                customer.DiaChi = adress;
+                customer.DienThoai = phoneNumber;
+
+                dbCustomer.tblKhach2s.InsertOnSubmit(customer);
+                dbCustomer.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //4. httpPut để chỉnh sửa thông tin một khách hàng
+        [HttpPut]
+        public bool UpdateCustomer(string id, string name,
+       string adress, string phoneNumber)
+        {
+            try
+            {
+                DBCustomersDataContext dbCustomer = new DBCustomersDataContext("");
+                //Lấy mã khách đã có
+                tblKhach2 customer = dbCustomer.tblKhach2s.FirstOrDefault(x => x.Makhach == id);
+                if (customer == null) return false;
+                customer.Makhach = id;
+                customer.TenKhach = name;
+                customer.DiaChi = adress;
+                customer.DienThoai = phoneNumber;
+                dbCustomer.SubmitChanges();//Xác nhận chỉnh sửa
+            return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //5.httpDelete để xóa một Khách hàng
+        [HttpDelete]
+        public bool DeleteCustomer(string id)
+        {
+            try
+            {
+                DBCustomersDataContext dbCustomer = new DBCustomersDataContext("");
+                //Lấy mã khách đã có
+                tblKhach2 customer =
+               dbCustomer.tblKhach2s.FirstOrDefault(x => x.Makhach == id);
+                if (customer == null) return false;
+
+                dbCustomer.tblKhach2s.DeleteOnSubmit(customer);
+                dbCustomer.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ LTAPI/BTVN3/resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by BTVN3.rc
+//
+#define IDC_MYICON                      2
+#define IDD_BTVN3_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_BTVN3                       107
+#define IDI_SMALL                       108
+#define IDC_BTVN3                       109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDC_CBTIN                       1000
+#define IDC_CBKYTHUAT                   1001
+#define IDC_XAYDUNG                     1002
+#define IDC_RDTHANHPHO                  1003
+#define IDC_RDDOTHI                     1004
+#define IDC_LIST1                       1005
+#define IDC_LSTMON                      1005
+#define IDC_CHAPNHAN                    1006
+#define IDC_LAMLAI                      1007
+#define IDC_TIMKIEM                     1008
+#define IDC_Thoat                       1009
+#define IDC_EXIT                        1009
+#define ID_TRANGCH32771                 32771
+#define ID_TRANGCH32772                 32772
+#define ID_TRANGCH32773                 32773
+#define ID_DKHOC                        32774
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
